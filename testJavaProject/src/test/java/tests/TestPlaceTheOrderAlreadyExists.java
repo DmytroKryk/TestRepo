9cf39decc12d118822282ff9c2d60fc5399dd4e3
DmytroKryk <dmitriy.krykun@gmail.com>
2025-04-25 16:41:22 +0300

    Minor refactoring

--- testJavaProject/src/test/java/tests/TestPlaceTheOrderAlreadyExists.java
+++ testJavaProject/src/test/java/tests/TestPlaceTheOrderAlreadyExists.java
@@ -6,57 +6,28 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utils.OrderDetails;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class TestPlaceTheOrderAlreadyExists extends TestBase {
 
     final int idToOrder = 8;
-    public String today = "";
 
     @BeforeMethod
     public void createOrder() {
-        try {
-            OrderDetails orderData = new OrderDetails();
-            orderData.orderDetails(idToOrder, 1, 1, "placed");
+        OrderDetails orderData = new OrderDetails();
+        String request = orderData.requestConstructor(idToOrder, 1, 1, "placed");
 
-            LocalDateTime todayDate = LocalDateTime.now();
-            DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            today = todayDate.format(myFormat);
+        Response response = sendPostRequest("/store/order", request);
+        Assert.assertEquals(response.getStatusCode(), 200);
 
-
-            String request = "{\n" +
-                    "  \"id\": " + orderData.getId() + ",\n" +
-                    "  \"petId\": " + orderData.getPetId() + ",\n" +
-                    "  \"quantity\": " + orderData.getQuantity() + ",\n" +
-                    "  \"shipDate\": \""+today+"T00:00:00.123Z\",\n" +
-                    "  \"status\": \"" + orderData.getStatus() + "\",\n" +
-                    "  \"complete\": true\n" +
-                    "}";
-
-            Response response = sendPostRequest("/store/order", request);
-            Assert.assertEquals(response.getStatusCode(), 200);
-
-            Thread.sleep(2000);//need to wait until test data posted
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //Thread.sleep(2000);//need to wait until test data posted
     }
 
     @Test(priority = 3)
     public void testPlaceTheOrderWhichAlreadyExists() {
 
         OrderDetails orderData = new OrderDetails();
-        orderData.orderDetails(idToOrder, 1, 1, "placed");
-
-        String request ="{\n" +
-                "  \"id\": "+orderData.getId()+",\n" +
-                "  \"petId\": "+orderData.getPetId()+",\n" +
-                "  \"quantity\": "+orderData.getQuantity()+",\n" +
-                "  \"shipDate\": \""+today+"T00:00:00.123Z\",\n" +
-                "  \"status\": \""+orderData.getStatus()+"\",\n" +
-                "  \"complete\": true\n" +
-                "}";
+        String request = orderData.requestConstructor(idToOrder, 1, 1, "placed");
 
         Response response = sendPostRequest("/store/order", request);
         Assert.assertEquals(response.getStatusCode(), 200);
